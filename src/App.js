eeacc9c4465aed16da67df302e8ec96ff1c2ec07
Иван <kleimenov416@gmail.com>
2021-03-05 11:01:11 -0500

    linked bootstrap, and add couple of classes, styling our app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import State from "./Components/State";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h3>Hello Hook!</h3>
+        <h3 className="text-secondary">Hello Hook!</h3>
+        <ul className="list-unstyled text-secondary">
+          <State />
+        </ul>
       </div>
     </div>
   );
---
+++ src/Components/State.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const State = () => {
+
+    return (
+        <li>
+            <h4>1. userState hook</h4>
+        </li>
+    )
+}
+
+export default State;
